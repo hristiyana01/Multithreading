22cfe30fbd6509a57da07e84b99bf10b7b627a62
hristiyana <96460833+hristiyana01@users.noreply.github.com>
2023-11-02 06:51:47 +0200

    testing commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+
+        MultiThread multiThread = new MultiThread();
+        MultiThread multiThread2 = new MultiThread();
+        multiThread.run();
+        multiThread2.run();
         System.out.println("Hello world!");
     }
 }
